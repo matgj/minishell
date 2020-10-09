5853ce558d8b4ccb34064de9cc5bd21e0630e7b0
Mathis <Mathis@MBPdeGrosjean2.Home>
2020-10-09 10:54:26 +0100

    parsing.c et parsing2.c normed

--- parsing.c
+++ parsing.c
@@ -6,114 +6,121 @@
 /*   By: Mathis <Mathis@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/24 10:49:39 by Mathis            #+#    #+#             */
-/*   Updated: 2020/10/08 16:33:50 by Mathis           ###   ########.fr       */
+/*   Updated: 2020/10/09 10:44:22 by Mathis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-t_cmds    parse_cmd(char *cmds_pipe) 
+t_cmds	parse_cmd(char *cmds_pipe)
 {
-    char    **token;
-    int     i;
-    t_cmds   cmds;
+	char	**token;
+	int		i;
+	t_cmds	cmds;
 
-    i = 0;
-    parse_token(&token, cmds_pipe);
-    while (token[i])
-            i++;
-    if(!(cmds.argv = ft_calloc(i + 1, sizeof(char *))))
-        return (cmds);
-    cmds.argc = i;
-    i = 0;
-    while(token[i])
-    {
-         cmds.argv[i] = token[i];
-         insert_actions(cmds.argv[i]);
-         i++;
-    } 
-    free(token);
-    i = 0;
-    while (i < ARG_MAX)
-        cmds.output[i++] = -1;
-    command_management(&cmds);
-    return (cmds);
+	i = 0;
+	parse_token(&token, cmds_pipe);
+	while (token[i])
+		i++;
+	if (!(cmds.argv = ft_calloc(i + 1, sizeof(char *))))
+		return (cmds);
+	cmds.argc = i;
+	i = 0;
+	while (token[i])
+	{
+		cmds.argv[i] = token[i];
+		insert_actions(cmds.argv[i]);
+		i++;
+	}
+	free(token);
+	i = 0;
+	while (i < ARG_MAX)
+		cmds.output[i++] = -1;
+	command_management(&cmds);
+	return (cmds);
 }
 
-t_cmds   *parse_pipe(char *cmds_semi)
+t_cmds	*parse_pipe2(char *cmds_semi)
 {
-    t_cmds   *cmds;
-    char    **cmds_pipe;
-    int     cpt; 
-    t_cmds   nul;
+	t_cmds	*cmds;
+	char	**cmds_pipe;
+	int		cpt;
+	t_cmds	nul;
 
-    cpt = 0;
-    cmds_pipe = ft_split(cmds_semi, '|');
-    free(cmds_semi);
-    while(cmds_pipe[cpt])
-            cpt++;
-    if(!(cmds = ft_calloc(sizeof(t_cmds), cpt + 1)))
-        return (NULL); 
-    cpt = 0;
-    while(cmds_pipe[cpt])
-    {
-        cmds[cpt] = parse_cmd(cmds_pipe[cpt]);
-        cpt++;
-    }
-    free(cmds_pipe);
-    nul.name = NULL;
-    cmds[cpt] = nul;
-    command_plug(cmds);
-    return (cmds);
+	cpt = 0;
+	cmds_pipe = ft_split(cmds_semi, '|');
+	free(cmds_semi);
+	while (cmds_pipe[cpt])
+		cpt++;
+	if (!(cmds = ft_calloc(sizeof(t_cmds), cpt + 1)))
+		return (NULL);
+	cpt = 0;
+	while (cmds_pipe[cpt])
+	{
+		cmds[cpt] = parse_cmd(cmds_pipe[cpt]);
+		cpt++;
+	}
+	free(cmds_pipe);
+	nul.name = NULL;
+	cmds[cpt] = nul;
+	command_plug(cmds);
+	return (cmds);
 }
+
+void	parsing_pipe(char *cmds_semi)
+{
+	int		l;
+	int		i;
+	t_cmds	*cmds_pipe;
+
+	l = 0;
+	while (cmds_semi[l])
+	{
+		i = 0;
+		cmds_pipe = parse_pipe2(&cmds_semi[l++]);
+		while (cmds_pipe[i].name)
+			command_exec(cmds_pipe[i++]);
+		check_pid();
+		free(cmds_pipe);
+	}
+}
+
 /*
-//d'abord on decoupe la ligne lue par gnl en tableau de ligne de commandes, 
-//chaque ";" dans une ligne constitue une ligne de commande à executer independamment des autres 
-//ls -l | cat -e ; echo my name is $USER ==> cest comme si dabord on entrait ls -l dans le terminal et qu'on appuie sur ENTER
-//puis qu'on entrait echo my name is $USER, ce sont deux commandes totalement distinctes
+**d'abord on decoupe la ligne lue par gnl en tableau de ligne de commandes,
+**chaque ";" dans une ligne constitue une ligne de commande à
+**executer independamment des autres
+**ls -l | cat -e ; echo my name is $USER ==> cest comme si dabord on
+**entrait ls -l dans le terminal et qu'on appuie sur ENTER
+**puis qu'on entrait echo my name is $USER, ce sont deux
+**commandes totalement distinctes
 */
 
-void    parsing(char *line)
+void	parsing(char *line)
 {
-    char    **cmds_semi;
-    t_cmds  *cmds_pipe;
-    char    *line_env;
-    int     l;
-    int     i;
+	char	**cmds_semi;
+	char	*line_env;
 
-    l = 0;
-    quotes(line);
-    line_env = var_env(line);
-    if (!syntax_error(line_env))
-    {
-        print_syntax_err(line);
-        return ;
-    }
-   //free(line);
-    cmds_semi = ft_split(line_env,';');
-    free(line_env);
-    while (cmds_semi[l]) 
-    {
-        i = 0;
-        cmds_pipe = parse_pipe(cmds_semi[l++]);
-        while (cmds_pipe[i].name)
-        { 
-               command_exec(cmds_pipe[i]);
-               i++;
-        }
-        check_pid();
-        free(cmds_pipe);
-    }
-    free(cmds_semi);
+	quotes(line);
+	line_env = var_env(line);
+	if (!syntax_error(line_env))
+	{
+		print_syntax_err(line);
+		return ;
+	}
+	//free(line);
+	cmds_semi = ft_split(line_env,';');
+	free(line_env);
+	parsing_pipe(*cmds_semi);
+	free(cmds_semi);
 }
 
-void	read_line(char **line) 
+void	read_line(char **line)
 {
-	int 	ret;
+	int		ret;
 	char	buf[1];
 
 	ret = get_next_line(1, line);
-	if(!ret && !ft_strlen(*line))
+	if (!ret && !ft_strlen(*line))
 	{
 		free(*line);
 		exit_shell(g_shell.status, 1);
--- parsing2.c
+++ parsing2.c
@@ -6,7 +6,7 @@
 /*   By: Mathis <Mathis@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/24 10:50:01 by Mathis            #+#    #+#             */
-/*   Updated: 2020/10/08 16:25:56 by Mathis           ###   ########.fr       */
+/*   Updated: 2020/10/09 10:54:08 by Mathis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,85 +14,89 @@
 
 extern t_shell g_shell;
 /*
-//quel type de chevron on a ? pour savoir comment rediriger le resultat des commandes 
-// >  0 redirec the output --stdin (0) (overwrite le fichier)
-// <  1 redicec the input --stout (1);
-// >> 2 redic the ouput stind with append (s'ajoute au fichier);
-//pas de chevron = 3
+**quel type de chevron on a ? pour savoir comment
+**rediriger le resultat des commandes
+** >  0 redirec the output --stdin (0) (overwrite le fichier)
+** <  1 redicec the input --stout (1);
+** >> 2 redic the ouput stind with append (s'ajoute au fichier);
+**pas de chevron = 3
 */
 
-void     which_chevron(t_cmds *cmds, char *argv) 
+void	which_chevron(t_cmds *cmds, char *argv)
 {
-     if (!ft_strcmp(argv, ">"))
-             cmds->chevron = 0;
-     else if (!ft_strcmp(argv, "<"))
-             cmds->chevron = 1;
-     else if (!ft_strcmp(argv, ">>"))        
-             cmds->chevron = 2;
-     else
-        cmds->chevron = 3;
+	if (!ft_strcmp(argv, ">"))
+		cmds->chevron = 0;
+	else if (!ft_strcmp(argv, "<"))
+		cmds->chevron = 1;
+	else if (!ft_strcmp(argv, ">>"))
+		cmds->chevron = 2;
+	else
+		cmds->chevron = 3;
 }
 
 /*
-//le resultat (output) de ma commande doit etre redirigee vers un fichier plutot que stout 
-//(exemple wc -l doc.c > hello.txt cree le fichier hello et y ecrit le nombre de ligne de doc.c)
-// le double chevron cest pareil mais ca s ajoute au contenu du fichier hello.txt si ca existe deja
-//je cree donc un nouveau FD
+**le resultat (output) de ma commande doit etre
+**redirigee vers un fichier plutot que stout
+**(exemple wc -l doc.c > hello.txt cree le fichier
+**hello et y ecrit le nombre de ligne de doc.c)
+** le double chevron cest pareil mais ca s ajoute au
+**contenu du fichier hello.txt si ca existe deja
+**je cree donc un nouveau FD
 */
 
-void    redirec_output(t_cmds *cmds, char *argv)
+void	redirec_output(t_cmds *cmds, char *argv)
 {
-    int fd;
+	int	fd;
 
-    g_shell.redir = g_shell.redir + 1;
-    if (cmds->chevron == 0)
-    {
-        if ((fd = open(argv, O_TRUNC | O_RDWR | O_CREAT, 0644)) < 0)
-            ft_printf("error creation open function redirec > output \n");
-        else
-            ft_tab_output(cmds->output, fd);
-    }
-    else if (cmds->chevron == 2)
-    {
-        if ((fd = open(argv, O_RDWR | O_CREAT | O_APPEND, 0644)) < 0)
-            ft_printf("error creation open function redirec >> ouput \n");
-        else   
-             ft_tab_output(cmds->output, fd);
-    }
+	g_shell.redir = g_shell.redir + 1;
+	if (cmds->chevron == 0)
+	{
+		if ((fd = open(argv, O_TRUNC | O_RDWR | O_CREAT, 0644)) < 0)
+			ft_printf("error creation open function redirec > output \n");
+		else
+			ft_tab_output(cmds->output, fd);
+	}
+	else if (cmds->chevron == 2)
+	{
+		if ((fd = open(argv, O_RDWR | O_CREAT | O_APPEND, 0644)) < 0)
+			ft_printf("error creation open function redirec >> ouput \n");
+		else
+			ft_tab_output(cmds->output, fd);
+	}
 }
 
 /*
-//largument (input) de ma commande est modifie par <, cela devient le fichier de droite
-//(wc -l < salut.txt va lister nb de ligne de salut.txt)
-//je cree donc un nouveau FD
+**largument (input) de ma commande est
+**modifie par <, cela devient le fichier de droite
+**(wc -l < salut.txt va lister nb de ligne de salut.txt)
+**je cree donc un nouveau FD
 */
 
-void    redirection(t_cmds *cmds)
+void	redirection(t_cmds *cmds)
 {
-    int i;
-    int fd;
+	int	i;
+	int	fd;
 
-    i = 0;
-    while (i < cmds->argc)
-    {
-        which_chevron(cmds, cmds->argv[i]);
-        if (cmds->chevron < 3)
-        {
-            if (cmds->chevron == 1)
-               {
-                    if ((fd = open(cmds->argv[i + 1], O_RDONLY)) < 0)
-                      ft_printf("error open function redirec < input\n");
-                    cmds->input = fd;
-                }
-             else 
-                redirec_output(cmds, cmds->argv[i + 1]);
-            free(cmds->argv[i]);
-            free(cmds->argv[i + 1]);
-            cmds->argv[i] = NULL;
-            cmds->argv[i + 1] = NULL;
-            i++;
-        }
-        i++;
-    }
-    command_clean(cmds);
-} 
+	i = -1;
+	while (++i < cmds->argc)
+	{
+		which_chevron(cmds, cmds->argv[i]);
+		if (cmds->chevron < 3)
+		{
+			if (cmds->chevron == 1)
+			{
+				if ((fd = open(cmds->argv[i + 1], O_RDONLY)) < 0)
+					ft_printf("error open function redirec < input\n");
+				cmds->input = fd;
+			}
+			else
+				redirec_output(cmds, cmds->argv[i + 1]);
+			free(cmds->argv[i]);
+			free(cmds->argv[i + 1]);
+			cmds->argv[i] = NULL;
+			cmds->argv[i + 1] = NULL;
+			i++;
+		}
+	}
+	command_clean(cmds);
+}
