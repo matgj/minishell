68c3e309af973d5ea098804124b8e33107d997f6
Your Name <you@example.com>
2020-10-29 10:04:54 +0100

    reglage -n -n -n

--- builtins/ft_echo.c
+++ builtins/ft_echo.c
@@ -5,13 +5,18 @@ int		ft_echo(t_cmds cmds)
 	int 		i;
 	int 		nflag;
 
-
 	if (!cmds.argv[1])
 		write(1, "\n", 1);
 	else
 	{
-		nflag = (!ft_strcmp("-n", cmds.argv[1])) ? 1 : 0;
-		i = (nflag == 1) ? 2 : 1;
+		i = 0;
+		nflag = 0;
+		while (i < cmds.argc)
+		{
+			nflag += (!ft_strcmp("-n", cmds.argv[i])) ? 1 : 0;
+			i++;
+		}
+		i = (nflag > 0) ? nflag + 1 : 1;
 		while (i < cmds.argc)
 		{
 			ft_putstr_fd(cmds.argv[i++], 1);
